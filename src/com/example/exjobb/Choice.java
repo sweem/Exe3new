47b990f81142a6163c9a6266b643f22f7de0ebdc
E <emmzie84@gmail.com>
2013-06-11 12:59:08 +0200

    Updated Choice
    Updated Choice with an constructor for using medicines.

--- src/com/example/exjobb/Choice.java
+++ src/com/example/exjobb/Choice.java
@@ -3,6 +3,8 @@ package com.example.exjobb;
 public class Choice {
 	public int icon;
 	public String title;
+	public String distance;
+	public String hours;
 	
 	public Choice() {
 		super();
@@ -12,5 +14,13 @@ public class Choice {
 		super();
 		this.icon = icon;
 		this.title = title;
+	}
+	
+	public Choice(int icon, String title, String distance, String hours){
+		super();
+		this.icon = icon;
+		this.title = title;
+		this.distance = distance;
+		this.hours = hours;
 	}
 }
