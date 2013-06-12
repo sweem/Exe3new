ea04fab877cf1181b215d3250c8cc261f0b31df2
E <emmzie84@gmail.com>
2013-06-12 11:57:15 +0200

    Added database connection to PharmaciesActitivy

--- src/com/example/exjobb/PharmaciesActivity.java
+++ src/com/example/exjobb/PharmaciesActivity.java
@@ -1,8 +1,19 @@
 package com.example.exjobb;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,6 +24,7 @@ import android.widget.AdapterView.OnItemClickListener;
 public class PharmaciesActivity extends Activity {
 	double dist;
 	private ListView lstView;
+	DBAdapter db;
 	//double latA, lonA, latB, lonB;
 	
 	
@@ -20,6 +32,36 @@ public class PharmaciesActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.pharmacies2);
+		
+		db = new DBAdapter(this);
+        try {
+            String destPath = "/data/data/" + getPackageName() +
+                "/databases";
+            File f = new File(destPath);
+            if (!f.exists()) {            	
+            	f.mkdirs();
+                f.createNewFile();
+            	
+            	//---copy the db from the assets folder into 
+            	// the databases folder---
+                CopyDB(getBaseContext().getAssets().open("mydb"),
+                    new FileOutputStream(destPath + "/MyDB"));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+       
+        db.open();
+        //ArrayList<String> arr = db.getAllPharmaciesWithDrugId("2");
+        //String[] arr = db.getAllPharmacyIdWithDrugId("2");
+        //Toast.makeText(getBaseContext(), "Arr has size " + arr.size(), Toast.LENGTH_SHORT).show();
+        /*for(int i = 0; i < arr.size(); i++) {
+        	Toast.makeText(getBaseContext(), "Pharmacy " + arr.get(i) + " with drug 1 in stock. ", Toast.LENGTH_SHORT).show();
+        }*/
+        
+        db.close();
 		
 		Choice choices[] = new Choice[] {
 				new Choice(R.drawable.apotek_ikon, "Apotek Svanen", "1,60 km", "08:00-18:00"), 
@@ -59,6 +101,18 @@ public class PharmaciesActivity extends Activity {
 		
 		Toast.makeText(getBaseContext(), "Distance between two points: " + dist + "km.", Toast.LENGTH_LONG).show();
 		
+	}
+	
+	public void CopyDB(InputStream inputStream, 
+		    OutputStream outputStream) throws IOException {
+		        //---copy 1K bytes at a time---
+		        byte[] buffer = new byte[1024];
+		        int length;
+		        while ((length = inputStream.read(buffer)) > 0) {
+		            outputStream.write(buffer, 0, length);
+		        }
+		        inputStream.close();
+		        outputStream.close();
 	}
 	
 	/* Haversine formula*/
