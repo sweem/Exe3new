b1301b735b5c26d19bd22de07253455f3596315e
E <emmzie84@gmail.com>
2013-06-25 13:01:42 +0200

    Updated AndroidManifest
    Updated AndroidManifest with a new activity for pharmacy details.

--- gen/com/example/exjobb/R.java
+++ gen/com/example/exjobb/R.java
@@ -29,9 +29,17 @@ public final class R {
     }
     public static final class drawable {
         public static final int apotek_ikon=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int recept_ikon=0x7f020002;
-        public static final int tablett_ikon=0x7f020003;
+        public static final int apoteket_ikon=0x7f020001;
+        public static final int apotekhjartat_ikon=0x7f020002;
+        public static final int apoteksg_ikon=0x7f020003;
+        public static final int curaa_ikon=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int kronansd_ikon=0x7f020006;
+        public static final int lloydsa_ikon=0x7f020007;
+        public static final int medstop_ikon=0x7f020008;
+        public static final int recept_ikon=0x7f020009;
+        public static final int tablett_ikon=0x7f02000a;
+        public static final int varda_ikon=0x7f02000b;
     }
     public static final class id {
         public static final int action_forgot_password=0x7f09001a;
@@ -63,45 +71,49 @@ public final class R {
         public static final int txtTitle=0x7f090013;
     }
     public static final class layout {
-        public static final int drugs=0x7f030000;
-        public static final int login=0x7f030001;
-        public static final int login2=0x7f030002;
-        public static final int lstview_footer_row=0x7f030003;
-        public static final int lstview_header_row=0x7f030004;
-        public static final int lstview_header_row2=0x7f030005;
-        public static final int lstview_item_row=0x7f030006;
-        public static final int lstview_item_row2=0x7f030007;
-        public static final int main=0x7f030008;
-        public static final int pharmacies=0x7f030009;
-        public static final int pharmacies2=0x7f03000a;
-        public static final int spinner_item_row=0x7f03000b;
+        public static final int details=0x7f030000;
+        public static final int drugs=0x7f030001;
+        public static final int login=0x7f030002;
+        public static final int login2=0x7f030003;
+        public static final int lstview_footer_row=0x7f030004;
+        public static final int lstview_header_row=0x7f030005;
+        public static final int lstview_header_row2=0x7f030006;
+        public static final int lstview_header_row3=0x7f030007;
+        public static final int lstview_item_row=0x7f030008;
+        public static final int lstview_item_row2=0x7f030009;
+        public static final int main=0x7f03000a;
+        public static final int pharmacies=0x7f03000b;
+        public static final int pharmacies2=0x7f03000c;
+        public static final int spinner_item_row=0x7f03000d;
     }
     public static final class menu {
-        public static final int drugs=0x7f080000;
-        public static final int login=0x7f080001;
-        public static final int login2=0x7f080002;
-        public static final int main=0x7f080003;
-        public static final int pharmacies=0x7f080004;
+        public static final int details=0x7f080000;
+        public static final int drugs=0x7f080001;
+        public static final int login=0x7f080002;
+        public static final int login2=0x7f080003;
+        public static final int main=0x7f080004;
+        public static final int pharmacies=0x7f080005;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f05000c;
+        public static final int action_forgot_password=0x7f05000d;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f05000a;
-        public static final int action_sign_in_short=0x7f05000b;
+        public static final int action_sign_in_register=0x7f05000b;
+        public static final int action_sign_in_short=0x7f05000c;
         public static final int app_name=0x7f050000;
-        public static final int error_field_required=0x7f050011;
-        public static final int error_incorrect_password=0x7f050010;
-        public static final int error_invalid_email=0x7f05000e;
-        public static final int error_invalid_password=0x7f05000f;
+        public static final int error_field_required=0x7f050012;
+        public static final int error_incorrect_password=0x7f050011;
+        public static final int error_invalid_email=0x7f05000f;
+        public static final int error_invalid_password=0x7f050010;
         public static final int hello_world=0x7f050002;
-        public static final int login_progress_signing_in=0x7f05000d;
+        public static final int login_progress_signing_in=0x7f05000e;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f050008;
-        public static final int prompt_password=0x7f050009;
+        public static final int prompt_email=0x7f050009;
+        public static final int prompt_password=0x7f05000a;
         public static final int title_activity_dbadapter=0x7f050005;
+        public static final int title_activity_details=0x7f050007;
         public static final int title_activity_drugs=0x7f050004;
-        public static final int title_activity_login=0x7f050007;
+        public static final int title_activity_login=0x7f050008;
         public static final int title_activity_login2=0x7f050003;
         public static final int title_activity_pharmacies=0x7f050006;
     }
