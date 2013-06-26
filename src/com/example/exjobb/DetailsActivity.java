2248da2ca0e09b8e8f0d5f2fe99d2790efeea6c6
E <emmzie84@gmail.com>
2013-06-26 13:05:19 +0200

    Updated DetailsActivity
    Updated DetailsActivity with textviews for opening and closing hours.

--- src/com/example/exjobb/DetailsActivity.java
+++ src/com/example/exjobb/DetailsActivity.java
@@ -3,6 +3,7 @@ package com.example.exjobb;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class DetailsActivity extends Activity {
 
@@ -10,13 +11,25 @@ public class DetailsActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.details);
+		
+		TextView  tvHeader = (TextView) findViewById(R.id.txtHeader);
+		TextView  tvHWD = (TextView) findViewById(R.id.txtHWD);
+		//TextView  tvHSAT = (TextView) findViewById(R.id.txtHSAT);
+		///TextView  tvHSUN = (TextView) findViewById(R.id.txtHSUN);
+		
+		Bundle b = getIntent().getExtras();
+		Pharmacy ph = new Pharmacy(b.getString("id"), b.getString("chName"), b.getString("phName"), b.getString("addr"), b.getString("pCode"), b.getString("pArea"), b.getString("pNbr"), b.getString("opHWD"), b.getString("clHWD"), b.getString("opHSAT"), b.getString("clHSAT"), b.getString("opHSUN"), b.getString("clHSUN"), b.getString("lat"), b.getString("lon"), b.getFloat("distToPh"));
+		tvHeader.setText(ph.getPharmacyName());
+		tvHWD.setText(ph.opHWD + "-" + ph.clHWD);
+		//tvHSAT.setText("Lördag " + ph.opHSAT + "-" + ph.clHSAT);
+		//tvHSUN.setText("Söndag " + ph.opHSUN + "-" + ph.clHSUN);
 	}
 
-	@Override
+	/*@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.details, menu);
 		return true;
-	}
+	}*/
 
 }
