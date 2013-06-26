1a926e601f6ace06e2a7995870ae50e113df015a
E <emmzie84@gmail.com>
2013-06-26 13:08:51 +0200

    Added OrderByDistance
    Added OrderByDistance as a comparator for sorting different pharmacies
    depending on the distance to object.

---
+++ src/com/example/exjobb/OrderByDistance.java
@@ -0,0 +1,22 @@
+package com.example.exjobb;
+
+import java.util.Comparator;
+
+public class OrderByDistance implements Comparator<Pharmacy> {
+
+	@Override
+	public int compare(Pharmacy lhs, Pharmacy rhs) {
+		int retVal = 0;
+		if(lhs.distToPh > rhs.distToPh) {
+			retVal = 1;
+		}
+		else if(lhs.distToPh < rhs.distToPh) {
+			retVal = -1;
+		}
+		else if(lhs.distToPh == rhs.distToPh) {
+			retVal = 0;
+		}
+		return retVal;
+	}
+
+}
