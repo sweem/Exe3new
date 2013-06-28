dd6ebcea895a3e2e154334161f0910fa529f190a
E <emmzie84@gmail.com>
2013-06-28 12:59:24 +0200

    Updated Pharmacy
    Updated with a new constructor without distance.

--- src/com/example/exjobb/Pharmacy.java
+++ src/com/example/exjobb/Pharmacy.java
@@ -21,6 +21,25 @@ public class Pharmacy {
 	public float distToPh;
 	public int iconId;
 	
+	public Pharmacy(String rID, String chainName, String pharmacyName, String address, String postalCode, String postalArea, String phoneNbr, String openingHoursWD, String closingHoursWD, String openingHoursSAT, String closingHoursSAT, String openingHoursSUN, String closingHoursSUN, String latitude, String longitude) {
+		super();
+		this.id = rID;
+		this.chName = chainName;
+		this.phName = pharmacyName;
+		this.addr = address;
+		this.pCode = postalCode;
+		this.pArea = postalArea;
+		this.pNbr = phoneNbr;
+		this.opHWD = openingHoursWD;
+		this.clHWD = closingHoursWD;
+		this.opHSAT = openingHoursSAT;
+		this.clHSAT = closingHoursSAT;
+		this.opHSUN = openingHoursSUN;
+		this.clHSUN = closingHoursSUN;
+		this.lat = latitude;
+		this.lon = longitude;
+	}
+	
 	public Pharmacy(String rID, String chainName, String pharmacyName, String address, String postalCode, String postalArea, String phoneNbr, String openingHoursWD, String closingHoursWD, String openingHoursSAT, String closingHoursSAT, String openingHoursSUN, String closingHoursSUN, String latitude, String longitude, float distToPharmacy) {
 		super();
 		this.id = rID;
