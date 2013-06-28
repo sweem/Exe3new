973192238efb4385d7b0c195bd117ee609b66ba8
E <emmzie84@gmail.com>
2013-06-28 12:54:46 +0200

    Updated Details
    Updated details with directions so the person can find the pharmacy.

--- src/com/example/exjobb/DetailsActivity.java
+++ src/com/example/exjobb/DetailsActivity.java
@@ -1,12 +1,28 @@
 package com.example.exjobb;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 
 public class DetailsActivity extends Activity {
-
+	DBAdapter db;
+	String id;
+	double curLat, curLon;
+	Pharmacy ph;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,8 +36,37 @@ public class DetailsActivity extends Activity {
 		TextView tvA = (TextView) findViewById(R.id.txtA);
 		TextView tvPCA = (TextView) findViewById(R.id.txtPCA);
 		
-		Bundle b = getIntent().getExtras();
-		Pharmacy ph = new Pharmacy(b.getString("id"), b.getString("chName"), b.getString("phName"), b.getString("addr"), b.getString("pCode"), b.getString("pArea"), b.getString("pNbr"), b.getString("opHWD"), b.getString("clHWD"), b.getString("opHSAT"), b.getString("clHSAT"), b.getString("opHSUN"), b.getString("clHSUN"), b.getString("lat"), b.getString("lon"), b.getFloat("distToPh"));
+		db = new DBAdapter(this);
+        try {
+            String destPath = "/data/data/" + getPackageName() +
+                "/databases";
+            File f = new File(destPath);
+            if (!f.exists()) {            	
+            	f.mkdirs();
+                f.createNewFile();
+            	
+            	//---copy the db from the assets folder into 
+            	// the databases folder---
+                CopyDB(getBaseContext().getAssets().open("mydb"),
+                    new FileOutputStream(destPath + "/MyDB"));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+       
+        db.open();
+        Bundle b = getIntent().getExtras();
+        id = b.getString("id");
+        curLat = b.getDouble("curLat");
+        curLon = b.getDouble("curLon");
+        /*String msg = id + "/" + curLat + "/" + curLon;
+        Log.e("ID/Lat/Lon", msg);*/
+        
+        ph = db.getPharmacyWithId(id);
+        db.close();
+       
 		tvHeader.setText(ph.getPharmacyName());
 		tvHWD.setText(ph.getOpeningHoursWD());
 		tvHSAT.setText(ph.getOpeningHoursSAT());
@@ -39,4 +84,21 @@ public class DetailsActivity extends Activity {
 		return true;
 	}*/
 	
+	public void onClickDirections(View view) {
+		Intent i = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("http://maps.google.com/maps?saddr=" + curLat + "," + curLon + "&daddr=" + ph.lat + "," + ph.lon));
+		startActivity(i);
+		//finish();
+	}
+	
+	public void CopyDB(InputStream inputStream, 
+		    OutputStream outputStream) throws IOException {
+		        //---copy 1K bytes at a time---
+		        byte[] buffer = new byte[1024];
+		        int length;
+		        while ((length = inputStream.read(buffer)) > 0) {
+		            outputStream.write(buffer, 0, length);
+		        }
+		        inputStream.close();
+		        outputStream.close();
+	}
 }
