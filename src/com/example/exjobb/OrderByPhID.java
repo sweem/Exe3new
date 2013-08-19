22ba7e9fc207c12ef8fc6fd01eb9978bedc2c23e
E <emmzie84@gmail.com>
2013-08-19 13:03:34 +0200

    Updated OrderByPhID

--- src/com/example/exjobb/OrderByPhID.java
+++ src/com/example/exjobb/OrderByPhID.java
@@ -4,19 +4,7 @@ import java.util.Comparator;
 
 public class OrderByPhID implements Comparator<Pharmacy> {
 	@Override
-	public int compare(Pharmacy lhs, Pharmacy rhs) {
-		/*int retVal = 0;
-		if(Integer.parseInt(lhs.id) > Integer.parseInt(rhs.id)) {
-			retVal = 1;
-		}
-		else if(Integer.parseInt(lhs.id) < Integer.parseInt(rhs.id)) {
-			retVal = -1;
-		}
-		else if(Integer.parseInt(lhs.id) == Integer.parseInt(rhs.id)) {
-			retVal = 0;
-		}
-		return retVal;*/
-		
+	public int compare(Pharmacy lhs, Pharmacy rhs) {	
 		return lhs.id.compareTo(rhs.id);
 	}
 }
