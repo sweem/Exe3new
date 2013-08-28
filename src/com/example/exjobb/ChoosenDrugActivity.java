3a819323255af05ec0bf0e2f1df5c15544006c9a
E <emmzie84@gmail.com>
2013-08-28 13:02:57 +0200

    Updated ChoosenDrugActivity
    Added method for next-button and a bundle to get info from the activity
    before this activity.

--- src/com/example/exjobb/ChoosenDrugActivity.java
+++ src/com/example/exjobb/ChoosenDrugActivity.java
@@ -2,14 +2,24 @@ package com.example.exjobb;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class ChoosenDrugActivity extends Activity {
-
+	String choosenDrugID, choosenNbr;
+	Boolean phWithoutDr;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.choosen_drug);
+		
+		Bundle b = getIntent().getExtras();
+		choosenDrugID = b.getString("drugID");
+		choosenNbr = b.getString("nbrOfDrug");
+		phWithoutDr = b.getBoolean("PhWithoutDr");
+		
 	}
 
 	@Override
@@ -19,4 +29,14 @@ public class ChoosenDrugActivity extends Activity {
 		return true;
 	}
 
+	public void onClickNext(View view) {
+		Intent i = new Intent(this, PharmaciesActivity2.class);
+		/*choosenDrugID = "9"; //Which drug to search for
+		choosenNbr = 1;*/ //Nbr of search drug
+		i.putExtra("drugID", choosenDrugID);
+		i.putExtra("nbrOfDrug", choosenNbr);
+		i.putExtra("phWithoutDr", false);
+		startActivity(i);
+		finish();
+	}
 }
