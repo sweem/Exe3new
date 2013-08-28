bd14846975f91d6b21ebb39d16077fa3736aba68
E <emmzie84@gmail.com>
2013-08-28 11:14:00 +0200

    Added ChoosenDrugActivity
    To show information about the choosen drug from DrugsActivity.

--- gen/com/example/exjobb/R.java
+++ gen/com/example/exjobb/R.java
@@ -85,48 +85,51 @@ public final class R {
         public static final int txtWP=0x7f09000b;
     }
     public static final class layout {
-        public static final int details=0x7f030000;
-        public static final int drugs=0x7f030001;
-        public static final int fragment_pharmacies2=0x7f030002;
-        public static final int fragment_pharmacies22=0x7f030003;
-        public static final int login=0x7f030004;
-        public static final int lstview_header_row=0x7f030005;
-        public static final int lstview_header_row2=0x7f030006;
-        public static final int lstview_item_row=0x7f030007;
-        public static final int lstview_item_rowwd=0x7f030008;
-        public static final int lstview_item_rowwod=0x7f030009;
-        public static final int main=0x7f03000a;
-        public static final int pharmacies=0x7f03000b;
-        public static final int pharmacies2=0x7f03000c;
+        public static final int choosen_drug=0x7f030000;
+        public static final int details=0x7f030001;
+        public static final int drugs=0x7f030002;
+        public static final int fragment_pharmacies2=0x7f030003;
+        public static final int fragment_pharmacies22=0x7f030004;
+        public static final int login=0x7f030005;
+        public static final int lstview_header_row=0x7f030006;
+        public static final int lstview_header_row2=0x7f030007;
+        public static final int lstview_item_row=0x7f030008;
+        public static final int lstview_item_rowwd=0x7f030009;
+        public static final int lstview_item_rowwod=0x7f03000a;
+        public static final int main=0x7f03000b;
+        public static final int pharmacies=0x7f03000c;
+        public static final int pharmacies2=0x7f03000d;
     }
     public static final class menu {
-        public static final int details=0x7f080000;
-        public static final int drugs=0x7f080001;
-        public static final int login=0x7f080002;
-        public static final int main=0x7f080003;
-        public static final int pharmacies=0x7f080004;
-        public static final int pharmacies_activity2=0x7f080005;
+        public static final int choosen_drug=0x7f080000;
+        public static final int details=0x7f080001;
+        public static final int drugs=0x7f080002;
+        public static final int login=0x7f080003;
+        public static final int main=0x7f080004;
+        public static final int pharmacies=0x7f080005;
+        public static final int pharmacies_activity2=0x7f080006;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f050011;
+        public static final int action_forgot_password=0x7f050012;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f05000f;
-        public static final int action_sign_in_short=0x7f050010;
+        public static final int action_sign_in_register=0x7f050010;
+        public static final int action_sign_in_short=0x7f050011;
         public static final int app_name=0x7f050000;
-        public static final int error_field_required=0x7f050016;
-        public static final int error_incorrect_password=0x7f050015;
-        public static final int error_invalid_email=0x7f050013;
-        public static final int error_invalid_password=0x7f050014;
+        public static final int error_field_required=0x7f050017;
+        public static final int error_incorrect_password=0x7f050016;
+        public static final int error_invalid_email=0x7f050014;
+        public static final int error_invalid_password=0x7f050015;
         public static final int hello_world=0x7f050002;
-        public static final int login_progress_signing_in=0x7f050012;
+        public static final int login_progress_signing_in=0x7f050013;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f05000d;
-        public static final int prompt_password=0x7f05000e;
+        public static final int prompt_email=0x7f05000e;
+        public static final int prompt_password=0x7f05000f;
+        public static final int title_activity_choosen_drug=0x7f05000c;
         public static final int title_activity_dbadapter=0x7f050005;
         public static final int title_activity_details=0x7f050007;
         public static final int title_activity_drugs=0x7f050004;
-        public static final int title_activity_login=0x7f05000c;
+        public static final int title_activity_login=0x7f05000d;
         public static final int title_activity_login2=0x7f050003;
         public static final int title_activity_pharmacies=0x7f050006;
         public static final int title_activity_pharmacies_activity2=0x7f050008;
