d9148574dcf975990f896777c3c12fae8153ea01
E <emmzie84@gmail.com>
2013-09-04 12:32:59 +0200

    Updated MainActivity
    Changed to three buttons from listview.

--- src/com/example/exjobb/MainActivity.java
+++ src/com/example/exjobb/MainActivity.java
@@ -1,35 +1,15 @@
 package com.example.exjobb;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import com.google.android.maps.GeoPoint;
-
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.Activity;
-import android.app.ListActivity;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.util.Log;
+import android.graphics.Color;
 import android.view.Menu;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-	private ListView lstView;
-	//GeoPoint p;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,38 +20,13 @@ public class MainActivity extends Activity {
 		//actionBar.setSubtitle("Undermeny");
 		actionBar.setTitle("Hitta din medicin");
 		
-		Choice choices[] = new Choice[] {
-				new Choice(R.drawable.tablett_ikon, "Hitta läkemedel"), 
-				new Choice(R.drawable.recept_ikon, "Mina recept"), 
-				new Choice(R.drawable.apotek_ikon, "Hitta apotek")
-				};
-		ChoiceArrayAdapter adapter = new ChoiceArrayAdapter(this, R.layout.lstview_item_row, choices);
+		Button btn1 = (Button) findViewById(R.id.btn1);
+		btn1.setBackgroundColor(Color.WHITE);
+		Button btn2 = (Button) findViewById(R.id.btn2);
+		btn2.setBackgroundColor(Color.WHITE);
+		Button btn3 = (Button) findViewById(R.id.btn3);
+		btn3.setBackgroundColor(Color.WHITE);
 		
-		lstView = (ListView) findViewById(R.id.lstView);
-		lstView.setAdapter(adapter);
-		
-		lstView.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
-				//Toast.makeText(getBaseContext(), "Has pos " + pos, Toast.LENGTH_LONG).show();
-				
-				switch(pos) {
-				case 0: startActivity(new Intent(MainActivity.this, DrugsActivity.class));
-						//finish();
-						break;
-				case 1: startActivity(new Intent(MainActivity.this, LoginActivity.class));
-						//finish();
-						break;
-				case 2: Intent i = new Intent(MainActivity.this, PharmaciesActivity2.class);
-						i.putExtra("phWithoutDr", true);
-						startActivity(i);
-						break;
-				}
-			}
-		});
-		
-		//TextView  tv = (TextView) findViewById(R.id.txtHeader);
-		//tv.setText("hehe");
 	}
 	
 	@Override
@@ -81,20 +36,17 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	/*public void onListItemClick (ListView parent, View v, int pos, long id) {
-		Choice item = (Choice) getListAdapter().getItem(pos);
-		Toast.makeText(this, "You've selected " + item.title, Toast.LENGTH_SHORT).show();
-	}*/
-	
-	/*public void onResume() {
-		super.onResume();
-		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
-		
-		lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, ll);
+	public void onClickDr(View view) {
+		startActivity(new Intent(MainActivity.this, DrugsActivity.class));
 	}
-
-	public void onPause() {
-		lm.removeUpdates(ll);
-	}*/
-
+	
+	public void onClickPr(View view) {
+		startActivity(new Intent(MainActivity.this, LoginActivity.class));
+	}
+	
+	public void onClickPh(View view) {
+		Intent i = new Intent(MainActivity.this, PharmaciesActivity2.class);
+		i.putExtra("phWithoutDr", true);
+		startActivity(i);
+	}
 }
