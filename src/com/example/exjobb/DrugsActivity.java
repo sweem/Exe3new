a64f352b3a97f140d75a94012f1aa1784c3d1a1f
E <emmzie84@gmail.com>
2013-09-04 12:34:19 +0200

    Updated DrugsActivity
    Uncommented the method onCreateOptionsMenu.

--- src/com/example/exjobb/DrugsActivity.java
+++ src/com/example/exjobb/DrugsActivity.java
@@ -314,12 +314,12 @@ public class DrugsActivity extends Activity implements OnItemSelectedListener {
 		db.close();
 	}
 	
-	@Override
+	/*@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.drugs, menu);
 		return true;
-	}
+	}*/
 	
 	public void doPositiveClick() {
 		//Log.d("DrugsActivity", "User clicks on OK");
