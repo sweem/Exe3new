997df82e64aeaa21dbdd70859834b47e5ee66b34
E <emmzie84@gmail.com>
2013-10-03 12:03:56 +0200

    Updated MainActivity2
    Deleted som imports.

--- src/com/example/exjobb/MainActivity2.java
+++ src/com/example/exjobb/MainActivity2.java
@@ -1,31 +1,14 @@
 package com.example.exjobb;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import com.google.android.maps.GeoPoint;
-
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.Activity;
-import android.app.ListActivity;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity2 extends Activity {
 	private ListView lstView;
