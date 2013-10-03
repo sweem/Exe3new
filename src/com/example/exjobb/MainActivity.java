21cd31b65013b09135a5489ce48857560789e75b
E <emmzie84@gmail.com>
2013-10-03 12:03:44 +0200

    Updated MainActivity
    Deleted som imports.

--- src/com/example/exjobb/MainActivity.java
+++ src/com/example/exjobb/MainActivity.java
@@ -4,10 +4,7 @@ import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends Activity {
 	
@@ -29,12 +26,12 @@ public class MainActivity extends Activity {
 		
 	}
 	
-	@Override
+	/*@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
+	}*/
 	
 	public void onClickDr(View view) {
 		startActivity(new Intent(MainActivity.this, DrugsActivity.class));
