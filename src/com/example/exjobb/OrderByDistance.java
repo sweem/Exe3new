e336545bc2dbc0f8bf356a9b55bbdd4cdfc7a9c0
E <emmzie84@gmail.com>
2014-08-15 11:26:12 +0200

    Updated OrderByDistance
    Updated with javadoc.

--- src/com/example/exjobb/OrderByDistance.java
+++ src/com/example/exjobb/OrderByDistance.java
@@ -2,6 +2,10 @@ package com.example.exjobb;
 
 import java.util.Comparator;
 
+/*
+ * The distance between two pharmacies is sorted in order.
+ */
+
 public class OrderByDistance implements Comparator<Pharmacy> {
 	@Override
 	public int compare(Pharmacy lhs, Pharmacy rhs) {
